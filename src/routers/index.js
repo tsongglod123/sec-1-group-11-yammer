3c491698859c0448c4bf6182e62eb499a6951b90
tan <t.songglod@gmail.com>
2022-04-13 21:45:10 +0700

    [+] import css

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+// import { createRouter, createWebHistory } from "vue-router";
 
-const history = createWebHistory();
-const routes = [
-	{
-		path: "/path",
-		name: "name",
-		component: component_name,
-	},
-];
+// const history = createWebHistory();
+// const routes = [
+// 	{
+// 		path: "/path",
+// 		name: "name",
+// 		component: component_name,
+// 	},
+// ];
 
-const router = createRouter({ history, routes });
+// const router = createRouter({ history, routes });
 
-export default router;
+// export default router;
