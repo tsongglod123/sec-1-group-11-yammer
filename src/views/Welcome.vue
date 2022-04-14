9804334f26ed250ea44ae1129b9e88c17c1ac9af
tan <t.songglod@gmail.com>
2022-04-15 03:07:10 +0700

    [update] addUsername function

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -18,6 +18,7 @@ const addUsername = async (new_username) => {
 		},
 		body: JSON.stringify({ username: new_username }),
 	});
+	login(new_username);
 };
 
 // GET
@@ -43,10 +44,6 @@ const login = async (name) => {
 			});
 		} else {
 			addUsername(name);
-			appRouter.push({
-				name: "homepage",
-				params: { userId: userId },
-			});
 		}
 		[username.value, isMatch.value] = ["", false];
 	}
@@ -54,38 +51,43 @@ const login = async (name) => {
 </script>
 
 <template>
-    <div id="welcome-page" style="font-family: 'Poppins', sans-serif;">
-        <div class="text-7xl project-title text-center pt-60 font-semibold" >Yammer</div>
-        <div class="grid justify-items-center py-8">
-            <div id="login" class="flex justify-items-center">
-				<div class="pr-5 ">
-                <input
-                    type="text"
-                    class="input input-lg rounded-full shadow-lg focus:outline-none bg-slate-100"
-                    placeholder="Username"
-                    v-model.trim="username"
-                    @keyup.enter="login($event.target.value)"
-                />
+	<div id="welcome-page" style="font-family: 'Poppins', sans-serif">
+		<div class="text-7xl project-title text-center pt-60 font-semibold">
+			Yammer
+		</div>
+		<div class="grid justify-items-center py-8">
+			<div id="login" class="flex justify-items-center">
+				<div class="pr-5">
+					<input
+						type="text"
+						class="input input-lg rounded-full shadow-lg focus:outline-none bg-slate-100"
+						placeholder="Username"
+						v-model.trim="username"
+						@keyup.enter="login($event.target.value)"
+					/>
 				</div>
-                <button
-                    type="button"
-                    class="btn btn-lg rounded-full shadow-lg border-none"
-                    @click.left="login(username)"
-                >
-                    Go
-                </button>
-            </div>
-            <div v-show="isNotFill" class="flex justify-items-center pt-5 text-red-600 ">
-                Please enter your username!
-            </div>
-        </div>
-    </div>
+				<button
+					type="button"
+					class="btn btn-lg rounded-full shadow-lg border-none"
+					@click.left="login(username)"
+				>
+					Go
+				</button>
+			</div>
+			<div
+				v-show="isNotFill"
+				class="flex justify-items-center pt-5 text-red-600"
+			>
+				Please enter your username!
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap");
 .btn {
-    @apply bg-teal-400 hover:bg-teal-500;
+	@apply bg-teal-400 hover:bg-teal-500;
 }
 .input {
 	@apply w-full max-w-xs;
