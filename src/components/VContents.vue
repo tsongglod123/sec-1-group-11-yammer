9900b731a4412af91a655356de12d0fb619432cb
DENPHUM\black <niklaz.soulless@gmail.com>
2022-04-16 00:33:39 +0700

    [EDIT] minor update:class

--- src/components/VContents.vue
+++ src/components/VContents.vue
@@ -15,17 +15,15 @@ const props = defineProps({
     <div id="content-list">
         <div class="hover-content">
             <div v-for="post in posts" :key="post.id">
-                <div class="box max-w-xl shadow-xl bg-white my-4 rounded-xl">
-                    <div class="box-border max-h-xl max-w-xl p-6">
-                        <div class="font-semibold text-xl">
-                            {{
-                                post.userId == userId
-                                    ? "You complain"
-                                    : "Someone complains"
-                            }}
-                        </div>
-                        <div class="break-words pt-2">{{ post.content }}</div>
+                <div class="box box-element box-border max-h-xl p-6 max-w-xl">
+                    <div class="font-semibold text-xl">
+                        {{
+                            post.userId == userId
+                                ? "You complain"
+                                : "Someone complains"
+                        }}
                     </div>
+                    <div class="break-words pt-2">{{ post.content }}</div>
                 </div>
             </div>
         </div>
@@ -33,10 +31,14 @@ const props = defineProps({
 </template>
 
 <style scoped>
+
+.box-element {
+    @apply shadow-xl bg-white my-4 rounded-xl;   
+}
 .hover-content:hover .box {
-    @apply scale-100 opacity-50 blur-sm duration-300 ease-in-out
+    @apply scale-100 opacity-50 blur-sm duration-100 ease-in-out;
 }
 .hover-content:hover .box:hover {
-    @apply blur-none scale-110 opacity-100
+    @apply blur-none scale-110 opacity-100;
 }
 </style>
