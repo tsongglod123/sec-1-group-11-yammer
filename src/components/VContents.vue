cdf351929ee11c750f447db3feed426881bca17c
tan <t.songglod@gmail.com>
2022-04-17 03:53:06 +0700

    [+] update :key in v-for (vcontents)

--- src/components/VContents.vue
+++ src/components/VContents.vue
@@ -1,35 +1,35 @@
 <script setup>
 const props = defineProps({
-    posts: {
-        type: Array,
-        require: true,
-    },
-    userId: {
-        type: Number,
-        require: true,
-    },
+	posts: {
+		type: Array,
+		require: true,
+	},
+	userId: {
+		type: Number,
+		require: true,
+	},
 });
 </script>
 
 <template>
-    <div id="content-list">
-        <div v-for="post in posts" :key="post.id">
-            <div class="box-element box-border max-h-xl p-6 max-w-xl">
-                <div class="font-semibold text-xl">
-                    {{
-                        post.userId === userId
-                            ? "You complain"
-                            : "Someone complains"
-                    }}
-                </div>
-                <div class="break-words pt-2">{{ post.content }}</div>
-            </div>
-        </div>
-    </div>
+	<div id="content-list">
+		<div v-for="(post, index) in posts" :key="index">
+			<div class="box-element box-border max-h-xl p-6 max-w-xl">
+				<div class="font-semibold text-xl">
+					{{
+						post.userId === userId
+							? "You complain"
+							: "Someone complains"
+					}}
+				</div>
+				<div class="break-words pt-2">{{ post.content }}</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 .box-element {
-    @apply shadow-xl bg-white my-3 rounded-xl;
+	@apply shadow-xl bg-white my-3 rounded-xl;
 }
 </style>
