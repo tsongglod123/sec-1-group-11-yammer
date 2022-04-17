9f6906cd2954fc7ef2f53291f2bc98481edf9919
hunnymc <thanaphon.mc@mail.kmutt.ac.th>
2022-04-18 00:32:03 +0700

    [EDIT] welcome page - Login div has center on every device.

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -52,37 +52,23 @@ const login = async (name) => {
 </script>
 
 <template>
-    <div
-        id="welcome-page"
-        style="font-family: 'Poppins', sans-serif"
-        class="content-container"
-    >
-        <div
-            class="fade-animation text-7xl project-title text-center pt-60 font-extrabold"
-        >
-            <span
-                class="logo"
-                >Yammer</span
-            >
-        </div>
-        <div class="fade-animation-delay grid justify-items-center py-8">
-            <div id="login" class="flex justify-items-center">
-                <div class="pr-5">
-                    <input
-                        type="text"
-                        class="input input-lg rounded-full shadow-lg focus:outline-none"
-                        placeholder="Username"
-                        v-model.trim="username"
-                        @keyup.enter="login($event.target.value)"
-                    />
-                </div>
-                <VLogin :username="username" @click:action="login" />
+    <div id="welcome-page" style="font-family: 'Poppins', sans-serif" class="content-container center-page">
+        <div class="center-page">
+            <div class="fade-animation text-7xl project-title 
+            text-center font-extrabold">
+                <span class="logo">Yammer</span>
             </div>
-            <div
-                v-show="isNotFill"
-                class="flex justify-items-center pt-5 text-red-600 font-semibold"
-            >
-                Please enter your username!
+            <div class="fade-animation-delay grid justify-items-center py-8">
+                <div id="login" class="flex justify-items-center">
+                    <div class="pr-5">
+                        <input type="text" class="input input-lg rounded-full shadow-lg focus:outline-none"
+                            placeholder="Username" v-model.trim="username" @keyup.enter="login($event.target.value)" />
+                    </div>
+                    <VLogin :username="username" @click:action="login" />
+                </div>
+                <div v-show="isNotFill" class="flex justify-items-center pt-5 text-red-600 font-semibold">
+                    Please enter your username!
+                </div>
             </div>
         </div>
     </div>
@@ -92,28 +78,40 @@ const login = async (name) => {
 .btn {
     @apply btn-primary rounded-full;
 }
+
 .input {
     @apply w-full max-w-xs;
     background-color: #EDE6DB;
 }
-.logo{
-   color: white;
+
+.logo {
+    color: white;
 }
+
 .content-container {
-    padding-top: 50px;
+    /* margin-top: auto; */
     text-align: center;
     height: 100%;
     width: 100%;
     position: absolute;
 }
+
 .fade-animation {
     opacity: 0;
     animation: opacity-in 1.5s forwards;
 }
+
 .fade-animation-delay {
     opacity: 0;
     animation: opacity-in 1s forwards;
-	animation-delay: 1s;
+    animation-delay: 1s;
+}
+
+.center-page {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 
 @keyframes opacity-in {
